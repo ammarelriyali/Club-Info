b3786e3cae3d1a51b53b0701d76c1f43a67c3d3c
Ammar El-Riyali <ammarelriyali@gmail.com>
2023-05-23 23:42:04 +0300

    finish first version of home screen

---
+++ Club Info/Views/Leagues/LeaguesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LeaguesViewController.swift
+//  Club Info
+//
+//  Created by ammar on 23/05/2023.
+//
+
+import UIKit
+
+class LeaguesViewController: UITableViewController {
+    var type :HomeType!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        switch(type){
+        
+        case .none:
+            <#code#>
+        case .some(_):
+            <#code#>
+            
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
